4d2f1b3453d2c220a858b7ddb4f74d303b71d223
ianaware <ian.w@aware.co.th>
2024-02-28 13:12:27 +0700

    Added program that ChatGPT asked me to make to test my knowledge.

---
+++ Data Structures/Week 4/average_list_2.py
@@ -0,0 +1,16 @@
+#Create a list of numbers
+
+num_list = [10, 5, 23, 56, 500]
+
+#Calculate the sum of the numbers
+
+num_list_sum = 0
+num_list_count = 0
+
+for numbers in num_list:
+    num_list_sum = num_list_sum + numbers
+
+num_list_count = len(num_list)
+
+print("The average is: ", num_list_sum / num_list_count)
+
