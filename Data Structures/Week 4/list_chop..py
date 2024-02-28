1a13a1c8847b7484e18e47531e66b71498c2bd80
ianaware <ian.w@aware.co.th>
2024-02-28 15:08:07 +0700

    Updated Latest

--- Data Structures/Week 4/list_chop..py
+++
@@ -1,4 +0,0 @@
-def chop(l):
-    del l[0] #delete first element
-    del l[-1] #delete last element (-1)
-    return None #return None
--- Data Structures/Week 4/list_middle.py
+++
@@ -1,3 +0,0 @@
-def middle (l): #Create function and take a list 'l'
-    rest = l[1:-1] #skip 0 (first and until the end -1)
-    return rest #return the new list
