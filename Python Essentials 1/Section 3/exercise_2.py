b888fd099cceeb5d15b4f08577308cb548ea55be
ianaware <ian.w@aware.co.th>
2024-03-12 15:05:45 +0700

    Started section 3
    More exercises

---
+++ Python Essentials 1/Section 3/exercise_2.py
@@ -0,0 +1,21 @@
+'''
+
+Estimated time
+5-10 minutes
+
+Level of difficulty
+Very Easy
+
+Objectives
+becoming familiar with the input() function;
+becoming familiar with comparison operators in Python.
+Scenario
+Using one of the comparison operators in Python, write a simple two-line program that takes the parameter n as input, which is an integer, and prints False if n is less than 100, and True if n is greater than or equal to 100.
+
+Don't create any if blocks (we're going to talk about them very soon). Test your code using the data we've provided for you.
+
+'''
+
+n_value = input("Enter a value: ")
+n_int = int(n_value)
+print(n_int >=100)
